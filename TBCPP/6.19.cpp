43b6455c2a8aa8072658f02813cf8f10606804c5
yekwangkim <ykk271@gmail.com>
2020-12-07 21:02:10 +0900

    .

---
+++ TBCPP/6.19.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int* ptr = nullptr;
+	int** ptrptr = nullptr;
+
+	int value = 5;
+
+	ptr = &value;
+	ptrptr = &ptr;
+
+	//cout << ptr << " " << *ptr << " " << &ptr << endl;
+	//cout << ptrptr << " " << *ptrptr << " " << &ptrptr << endl;
+
+	//cout << **ptrptr << endl;
+
+	return 0;
+}
