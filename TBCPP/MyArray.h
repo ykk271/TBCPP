c011bd2d3f1662e9d7f2659376c394beb1bd92a8
yekwangkim <ykk271@gmail.com>
2020-12-27 23:39:08 +0900

    .

---
+++ TBCPP/MyArray.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <assert.h>
+#include <iostream>
+
+template<typename T>
+class MyArray
+{
+private:
+	T m_length;
+	T* m_data;
+
+public:
+	MyArray()
+	{
+		m_length = 0;
+		m_data = nullptr;
+	}	
+
+	MyArray(int length)
+	{
+		m_data = new T[length];
+		m_length = length;
+	}
+
+	~MyArray()
+	{
+		reset();
+	}
+
+	void reset()
+	{
+		delete[] m_data;
+		m_data = nullptr;
+		m_length = 0;
+	}
+
+	T& operator[](int index)
+	{
+		assert(index >= 0 && index < m_length);
+		return m_data[index];
+	}
+
+	int getLength()
+	{
+		return m_length;
+	}
+
+	void print();
+};
+
+
+
